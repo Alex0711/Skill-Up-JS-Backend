60eddd5c928ad46070946db4411da82f8e7bbf8c
julandrod <julian.andres.rodriguez@gmail.com>
2022-11-12 10:41:13 -0500

    add: backoffice route

--- views/src/Components/animated/AnimateRoutes.jsx
+++ views/src/Components/animated/AnimateRoutes.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes, Route, useLocation } from "react-router-dom";
 import LandingPage from "../../pages/LandingPage";
 import DashboardPage from "../../pages/DashboardPage";
-// import FormLoginRegister from "./Components/Forms/FormLoginRegister";
 import LoginPage from "../../pages/LoginPage";
 import MovementPage from "../../pages/MovementPage";
 import MovementsPage from "../../pages/MovementsPage";
@@ -11,6 +10,7 @@ import ExpensePage from "../../pages/ExpensePage";
 import TransferPage from "../../pages/TransferPage";
 import About from '../About/About';
 import UserPage from "../../pages/UserPage";
+import BackOffice from '../../pages/BackofficePage';
 
 const AnimateRoutes = () => {
     const location = useLocation();
@@ -27,6 +27,7 @@ const AnimateRoutes = () => {
       <Route path="/charge" element={<BalanceChargePage />}/>
       <Route path="/expense" element={<ExpensePage />} />
       <Route path="/transfer" element={<TransferPage />} />
+      <Route path="/backoffice" element={<BackOffice />} />
       <Route path="*" element={<LandingPage fireSwal={true} />} /> {/* if no route matchs, redirects to landing */}
     </Routes>
   )
