2c71ca47f1f82c7839d3b3a3717d26b559fd6a5f
julandrod <julian.andres.rodriguez@gmail.com>
2022-11-12 10:37:36 -0500

    fix: small bug about key

--- views/src/Components/Forms/FormLoginRegister.jsx
+++ views/src/Components/Forms/FormLoginRegister.jsx
@@ -64,9 +64,8 @@ const FormLoginRegister = () => {
                             isLogin &&
                             (input.name === "firstName" ||
                               input.name === "lastName") ? null : (
-                              <div className="mb-4">
+                              <div className="mb-4" key={idx}>
                                 <FormItem
-                                  key={idx}
                                   classLabel="block mt-3"
                                   classInput="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                   type={input.type}
