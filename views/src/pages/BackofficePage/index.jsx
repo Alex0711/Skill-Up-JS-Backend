e7710b7cd0ff5c00d4c90b667ea40d0f82788687
Julian Rodriguez <juliancho_rodriguez@hotmail.com>
2022-11-12 11:33:31 -0500

    Update index.jsx

--- views/src/pages/BackofficePage/index.jsx
+++ views/src/pages/BackofficePage/index.jsx
@@ -6,7 +6,7 @@ import HeaderContainer from "../../Components/Header/HeaderContainer";
 const BackOffice = () => {
   return (
     <>
-      <HeaderContainer />
+      //<HeaderContainer />
       <main className="flex flex-col px-6 lg:px-16 py-8 gap-6">
         <h1 className="text-4xl font-bold">Admin Dashboard</h1>
         <BackOfficeContainer />
